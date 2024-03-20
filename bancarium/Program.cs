8e4611e13497dc77c3fef135f2a926095b094c75
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-20 15:09:46 +0100

    rimozione prestiti aggiunta

--- bancarium/Program.cs
+++ bancarium/Program.cs
@@ -10,17 +10,255 @@ La banca deve poter eseguire delle ricerche sui prestiti concessi ad un cliente 
 l’ammontare totale dei prestiti concessi.
  */
 
-
-
-
-
-
+using bancarium;
+using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+
+        Client cliente1 = new Client("mario", "rossi", "BASFWFG", 1000.00);
+        Client cliente2 = new Client("gianni", "verdi", "BASFWFG", 1020.50);
+        Client cliente3 = new Client("sara", "gialli", "BASFWFG", 1100.20);
+
+        clienti.Add(cliente3);
+        clienti.Add(cliente2);
+        clienti.Add(cliente1);
+
+
+        while (true)
+        {
+            Console.WriteLine("seleziona cosa vuoi fare\n1.aggiungi prestito\n2.rimuovi prestito \n3.modifica prestito \n4.ricerca un cliente \n5.vedi lista prestiti \n6.esci");
+            string input = Console.ReadLine();
+
+
+            bool exit = false;
+
+            switch (input)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("selezionare cliente");
+
+                        for(int i = 0; i < clienti.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + clienti[i].ToString());
+                        }
+                        
+                        while (true)
+                        {
+                            int clientSelector = CheckInt();
+                            try
+                            {
+                                    Prestito output = CreateLoan(clienti[clientSelector - 1]);
+                                    Console.WriteLine($"il prestito {output.ToString()} per {clienti[clientSelector - 1].presentazione()} è stato creato con successo");
+                                    break;
+                               
+                            } catch (ArgumentOutOfRangeException ex)
+                            {
+                                Console.WriteLine("numero non valido selezionare un numero nella lista");
+                            }
+                          
+
+                        }
+                        break;
+                    }
+
+                case "2":
+                    {
+                        for (int i = 0; i < prestiti.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + prestiti[i].ToString());
+                        }
+
+
+                        while (true)
+                        {
+                            int loanSelector = CheckInt();
+                            try
+                            {
+          
+                                    string output = DeleteLoan(loanSelector - 1);
+                                    Console.WriteLine(output);
+                                    break;
+                            } catch(ArgumentOutOfRangeException ex)
+                            {
+                                Console.WriteLine("numero non valido selezionare un numero nella lista");
+                            }
+                          
+
+
+
+                        }
+
+                        break;
+                    }
+
+
+                case "5":
+                    {
+
+                        for (int i = 0; i < prestiti.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + prestiti[i].ToString());
+                        }
+
+
+                        break;
+                    }
+                case "6":
+                    {
+                        Console.WriteLine("uscita...");
+                        exit = true;
+                        break;
+                    }
+
+
+                default:
+                    {
+                        Console.WriteLine("input non valido");
+                        break;
+                    }
+            }
+            if (exit)
+            {
+                break;
+            }
+
+
+
+
+
+        }
+
     }
+
+
+
+
+    
+
+
+    static int CheckInt()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("inserisci un numero intero");
+                int clientSelector = Convert.ToInt32(Console.ReadLine());
+                return clientSelector;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("input non valido");
+
+            }
+        }
+    }
+
+    static double CheckDouble()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("inserisci un numero con il \".\"");
+                double ammount = Convert.ToDouble(Console.ReadLine());
+                return ammount;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("input non valido");
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+    public static Prestito CreateLoan(Client cliente)
+    {
+        DateTime today = DateTime.Today;
+
+        Console.WriteLine("inserire la data nel seguente formato \"gg/MM/AAAA\"");
+        string line = Console.ReadLine();
+        DateTime dt;
+
+        while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+        {
+            Console.WriteLine("data invalida, per favore riprova");
+            line = Console.ReadLine();
+        }
+
+
+        Console.WriteLine("inserire tipo di rata \n 1.settimanale \n 2.mensile \n 3.annuale");
+        string rateType = Console.ReadLine();
+        string rata = "";
+        while (rata == "")
+        {
+            switch (rateType)
+            {
+                case "1":
+                    {
+                        rata = "settimanale";
+                        break;
+                    }
+                case "2":
+                    {
+                        rata = "mensile";
+                        break;
+                    }
+                case "3":
+                    {
+                        rata = "annuale";
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("input non valido");
+                        break;
+
+
+                    }
+            }
+        }
+
+        Console.WriteLine("inserire totale da restituire");
+
+        double ammontare = CheckDouble();
+
+
+        Prestito loan = new Prestito(today, dt, rata, ammontare, cliente);
+
+        prestiti.Add(loan);
+
+        return loan;
+
+
+    }
+
+    public static string DeleteLoan(int index)
+    {
+        prestiti.RemoveAt(index);
+
+
+        return "il prestito è stato eliminato corettamente";
+
+
+    }
+
+
+
+
+
 }
 
 
