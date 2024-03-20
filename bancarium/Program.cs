569c1a79e87f61c77af77db7dd27a5f4469a56cf
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-20 16:38:18 +0100

    modifiche ai prestiti aggiunta

--- bancarium/Program.cs
+++ bancarium/Program.cs
@@ -24,17 +24,34 @@ internal class Program
 
 
 
+
+
     private static void Main(string[] args)
     {
 
 
+        DateTime data1 = DateTime.Now;
+        DateTime data2 = new DateTime(02 / 10 / 2021);
+        DateTime data3 = new DateTime(08 / 05 / 2023);
+        DateTime data4 = new DateTime(09 / 12 / 2022);
+        DateTime data5 = new DateTime(09 / 10 / 2024);
+        DateTime data6 = new DateTime(09 / 10 / 2025);
+
         Client cliente1 = new Client("mario", "rossi", "BASFWFG", 1000.00);
-        Client cliente2 = new Client("gianni", "verdi", "BASFWFG", 1020.50);
-        Client cliente3 = new Client("sara", "gialli", "BASFWFG", 1100.20);
+        Client cliente2 = new Client("gianni", "verdi", "AFGGXHWG", 1020.50);
+        Client cliente3 = new Client("sara", "gialli", "VXGWTVSFG", 1100.20);
+        Prestito prestito1 = new Prestito(data1, data6, "settimanale", 402.23, cliente1);
+        Prestito prestito2 = new Prestito(data2, data5, "2 mesi", 4124124.42, cliente1);
+        Prestito prestito3 = new Prestito(data4, data5, "mensile", 524.42, cliente3);
+        Prestito prestito4 = new Prestito(data3, data5, "annuale", 48624.42, cliente2);
 
         clienti.Add(cliente3);
         clienti.Add(cliente2);
         clienti.Add(cliente1);
+        prestiti.Add(prestito1);
+        prestiti.Add(prestito2);
+        prestiti.Add(prestito3);
+        prestiti.Add(prestito4);
 
 
         while (true)
@@ -104,6 +121,35 @@ internal class Program
 
                         break;
                     }
+
+                case "3":
+                    {
+
+                        for (int i = 0; i < prestiti.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + prestiti[i].ToString());
+                        }
+
+
+                        while (true)
+                        {
+                            int loanSelector = CheckInt();
+                            try
+                            {
+
+                                Prestito output = PutLoan(prestiti[loanSelector - 1]);
+                                Console.WriteLine($"il prestito è stato midificato con successo ecco quello nuovo: {output.ToString()}");
+                                break;
+                            }
+                            catch (ArgumentOutOfRangeException ex)
+                            {
+                                Console.WriteLine("numero non valido selezionare un numero nella lista");
+                            }
+
+                        }
+
+                            break;
+                        }
 
 
                 case "5":
@@ -211,10 +257,10 @@ internal class Program
         }
 
 
-        Console.WriteLine("inserire tipo di rata \n 1.settimanale \n 2.mensile \n 3.annuale");
+        Console.WriteLine("inserire tipo di rata \n 1.settimanale \n 2.mensile \n 3.annuale \n 4.personalizzata");
         string rateType = Console.ReadLine();
         string rata = "";
-        while (rata == "")
+        while (rata == "" || rata == null)
         {
             switch (rateType)
             {
@@ -231,6 +277,12 @@ internal class Program
                 case "3":
                     {
                         rata = "annuale";
+                        break;
+                    }
+                case "4":
+                    {
+                        Console.WriteLine("inserire rata");
+                        rata = Console.ReadLine();
                         break;
                     }
                 default:
@@ -263,6 +315,168 @@ internal class Program
 
 
         return "il prestito è stato eliminato corettamente";
+
+
+    }
+
+
+
+    public static Prestito PutLoan(Prestito prestito)
+    {
+
+        bool exit = true;
+
+        while (exit)
+        {
+            Console.WriteLine("cosa vuoi modificare ? \n1.data d'inizio  \n2.data di fine \n3.tipo di rata \n4.ammontare \n5.per uscire");
+            string putSelector = Console.ReadLine();
+            switch (putSelector)
+            {
+                case "1":
+                    {
+
+                        Console.WriteLine("inserire la data nel seguente formato \"gg/MM/AAAA\" \n0.per annullare");
+                        string linea = Console.ReadLine();
+                        DateTime newDate;
+                        if (linea == "0")
+                        {
+                            linea = prestito.getDataDiInizio().ToString("dd/MM/yyyy");
+                            Console.WriteLine(linea);
+                        }
+
+                        while (!DateTime.TryParseExact(linea, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out newDate))
+                        {
+
+                            Console.WriteLine("data invalida, per favore riprova");
+                            linea = Console.ReadLine();
+                            if (linea == "0")
+                            {
+                                linea = prestito.getDataDiInizio().ToString("dd/MM/yyyy");
+                                Console.WriteLine(linea);
+                            }
+
+
+                        }
+
+                        prestito.setDataDiInizio(newDate);
+
+                        break;
+                    }
+
+                case "2":
+                    {
+
+                        Console.WriteLine("inserire la data nel seguente formato \"gg/MM/AAAA\"  \n0.per annullare");
+                        string line = Console.ReadLine();
+                        DateTime dt;
+
+                        if (line == "0")
+                        {
+                            line = prestito.getDataDiFine().ToString("dd/MM/yyyy");
+                            Console.WriteLine(line);
+                        }
+
+                        while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+                        {
+                            Console.WriteLine("data invalida, per favore riprova");
+                            line = Console.ReadLine();
+                            if (line == "0")
+                            {
+                                line = prestito.getDataDiFine().ToString();
+                            }
+                        }
+
+
+                        prestito.setDataDiFine(dt);
+
+                        break;
+                    }
+
+                case "3":
+                    {
+                        Console.WriteLine("inserire tipo di rata \n 1.settimanale \n 2.mensile \n 3.annuale \n 4.personalizzata \n5.annulla");
+                        string rateType = Console.ReadLine();
+                        string rata = "";
+                        while (rata == "" || rata == null)
+                        {
+                            switch (rateType)
+                            {
+                                case "1":
+                                    {
+                                        rata = "settimanale";
+                                        break;
+                                    }
+                                case "2":
+                                    {
+                                        rata = "mensile";
+                                        break;
+                                    }
+                                case "3":
+                                    {
+                                        rata = "annuale";
+                                        break;
+                                    }
+                                case "4":
+                                    {
+                                        Console.WriteLine("inserire rata");
+                                        rata = Console.ReadLine();
+                                        break;
+                                    }
+                                case "5":
+                                    {
+                                        rata = prestito.getRata();
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        Console.WriteLine("input non valido");
+                                        break;
+
+
+                                    }
+                            }
+                        }
+
+                        prestito.setRata(rata);
+
+                        break;
+                    }
+
+                case "4":
+                    {
+
+
+                        Console.WriteLine("inserire totale da restituire \n0.per annullare ");
+
+                        double ammontare = CheckDouble();
+
+                        if (ammontare == 0.0)
+                        {
+                            ammontare = prestito.getAmmontare();
+                        }
+
+                        prestito.setAmmontare(ammontare);
+
+                        break;
+                    }
+                case "5":
+                    {
+                        Console.WriteLine("uscita");
+                        exit = false;
+                        break;
+                    }
+
+
+
+            }
+
+
+
+
+        }
+
+        return prestito;
+
 
 
     }
